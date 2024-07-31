fccbcaa0b5757e675283c94ff5c2ac69ca1925da
Jamie Reardon <jamie.reardon91@gmail.com>
2024-07-31 15:56:52 +0100

    implement auth, appwrite, and sentry

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,6 +2,7 @@
 import { type ClassValue, clsx } from 'clsx';
 import qs from 'query-string';
 import { twMerge } from 'tailwind-merge';
+import { z } from 'zod';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -177,3 +178,19 @@ export const getTransactionStatus = (date: Date) => {
 
   return date > twoDaysAgo ? 'Processing' : 'Success';
 };
+
+export const authFormSchema = (type: string) =>
+  z.object({
+    // sign up
+    firstName: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+    lastName: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+    address1: type === 'sign-in' ? z.string().optional() : z.string().max(50),
+    city: type === 'sign-in' ? z.string().optional() : z.string().max(50),
+    county: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+    postcode: type === 'sign-in' ? z.string().optional() : z.string().min(6).max(6),
+    dob: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+    ssn: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+    // both
+    email: z.string().email(),
+    password: z.string().min(8)
+  });
