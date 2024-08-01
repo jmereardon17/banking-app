b4e148a30504b5e6e045360e46012bd43409ff7c
Jamie Reardon <jamie.reardon91@gmail.com>
2024-08-01 22:05:12 +0100

    fix validation errors for data format

--- lib/utils.ts
+++ lib/utils.ts
@@ -186,9 +186,9 @@ export const authFormSchema = (type: string) =>
     lastName: type === 'sign-in' ? z.string().optional() : z.string().min(3),
     address1: type === 'sign-in' ? z.string().optional() : z.string().max(50),
     city: type === 'sign-in' ? z.string().optional() : z.string().max(50),
-    county: type === 'sign-in' ? z.string().optional() : z.string().min(3),
-    postcode: type === 'sign-in' ? z.string().optional() : z.string().min(6).max(6),
-    dob: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+    state: type === 'sign-in' ? z.string().optional() : z.string().max(2),
+    postalCode: type === 'sign-in' ? z.string().optional() : z.string().min(3).max(6),
+    dateOfBirth: type === 'sign-in' ? z.string().optional() : z.string().min(3),
     ssn: type === 'sign-in' ? z.string().optional() : z.string().min(3),
     // both
     email: z.string().email(),
